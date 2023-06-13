9cd06ba2fcc1986636af2d05d0ca93b084154ab7
LaszAndrea <laszlo.andika1717@gmail.com>
2023-06-13 09:42:07 +0200

    commitka

---
+++ src/app/patient-list/patient-list.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PatientDTO } from 'models';
+import { ToastrService } from 'ngx-toastr';
+import { PatientService } from '../services/patient.service';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-patient-list',
+  templateUrl: './patient-list.component.html',
+  styleUrls: ['./patient-list.component.css']
+})
+export class PatientListComponent implements OnInit{
+    patients: PatientDTO[] = [];
+
+    constructor(
+      private patientService: PatientService,
+      private toastrService: ToastrService,
+      private router: Router) { }
+
+      ngOnInit(): void {
+        this.patientService.getAll().subscribe({
+          next: (products) => { this.patients = products },
+          error: (err) => {
+            this.toastrService.error('A termék lista betöltésekor hiba keletkezett.', 'Hiba');
+          }
+        });
+      }
+}
